09c38a7ebb4a6d19b4f57c73dc6d1ebb67d364d5
PLHsu <54821322+plhsu19@users.noreply.github.com>
2020-11-13 12:13:43 +0100

    add logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'tachyons';
+import logo from './Facer_logo.png';
 import './Logo.css'
 
 function Logo(props) {
@@ -7,8 +8,8 @@ function Logo(props) {
         // use in-line style css to add the style for "Sign Out"
         // in the JAX syntax, we actually pass a JS object as CSS style to the 
         // JAX elements
-        <div className='ma4 mt0'>
-        <img src={"./logo_transparent.png"} alt='logo'/>
+        <div className='ma4 mt0 logo'>
+            <img src={logo} alt='logo' style={{width: '20%', }}/>
         </div>
     )
 }
