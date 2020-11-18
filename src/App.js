182f6027d3bae17663d7be0e09b62c51ebc2c95d
PLHsu <54821322+plhsu19@users.noreply.github.com>
2020-11-18 23:30:27 +0100

    add particle.js background

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-
+import Particles from 'react-particles-js';
+import particleParameter from './particlePara';
 
 class App extends Component {
-
   render() {
     return (
       <div className="App">
+        <Particles params={particleParameter} className='particle' />
         <Navigation />
         <Logo />
         <Rank />
